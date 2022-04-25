4b35cf399913155474f295d7c91713421c7de8d9
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-04-25 23:59:18 +0200

    kinematics and odometry update

--- src/project1/src/kinematics.cpp
+++ src/project1/src/kinematics.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "geometry_msgs/TwistStamped"
+#include "sensor_msgs/JointState"
 
 #define WHEEL_RADIUS 0.07
 #define WHEEL_POSITION_ALONG_X 0.200
@@ -7,7 +8,12 @@
 #define GEAR_RATIO 5
 #define ENCODERS_RESOLUTION 42
 
-int main(int argc, char **argv) {
+void wheel_statesCallback(const sensor_msgs::JointState::ConstPtr& msg) {
+    kinematics(msg->velocity[0],msg->velocity[1],msg->velocity[2],msg->velocity[3]);
+
+}
+
+void kinematics(float front_left_velocity, float front_right_velocity, float rear_left_velocity, float rear_right_velocity,){
     float u_1;
     float u_2;
     float u_3;
@@ -16,18 +22,12 @@ int main(int argc, char **argv) {
     float v_y;
     float w;
 
-    ros::init(argc, argv, "kinematics");  //per inizializzare il nodo
-    ros::NodeHandle n;    //per inizializzare il nodo
-
     ros::Publisher kinematics_pub = n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);
 
-    u_1 = /*serve il dato del bag*/ * (1/60) * (1/GEAR_RATIO);
-    u_2 = /*serve il dato del bag*/ * (1/60) * (1/GEAR_RATIO);
-    u_3 = /*serve il dato del bag*/ * (1/60) * (1/GEAR_RATIO);
-    u_4 = /*serve il dato del bag*/ * (1/60) * (1/GEAR_RATIO);
-
-
-
+    u_1 = front_left_velocity * (1/60) * (1/GEAR_RATIO);
+    u_2 = front_right_velocity * (1/60) * (1/GEAR_RATIO);
+    u_3 = rear_left_velocity * (1/60) * (1/GEAR_RATIO);
+    u_4 = rear_right_velocity * (1/60) * (1/GEAR_RATIO);
 
 
     v_x = (WHEEL_RADIUS / 2) * (u_1 + u_2);
@@ -43,19 +43,27 @@ int main(int argc, char **argv) {
         velocities_msg.linear.x = v_x;
         velocities_msg.linear.y = v_y;
         velocities_msg.linear.z = 0;
-        velocities_msg.angular.x = ;
-        velocities_msg.angular.y = ;
-        velocities_msg.angular.z = ;
+        velocities_msg.angular.x = 0;
+        velocities_msg.angular.y = 0;
+        velocities_msg.angular.z = w;
 
-
-
-
+        //time and frame_id
 
         kinematics_pub.publish(velocities_msg);
 
         ros::spinOnce();    //per dare il permesso a ROS, se c'e' qualcosa da fare prima di continuare con il main loop (da fare sempre se non c'e' il calcolo parallelo
         loop_rate.sleep();  //per mantenere la frequenza
     }
+}
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "kinematics");  //per inizializzare il nodo
+    ros::NodeHandle n;                    //per inizializzare il nodo
+
+    ros::Subscriber kinematics_sub = n.subscribe("wheel_states", 1000, wheel_statesCallback);
+
+    ros::spin();     //solo ROS, e' piu' efficiente perche' non considera ulteriori funzioni
 
     return 0;
 }
--- src/project1/src/odometry.cpp
+++ src/project1/src/odometry.cpp
@@ -1,7 +1,24 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "geometry_msgs/TwistStamped"
+
+void cmd_velCallback(const geometry_msgs::TwistedStamped::ConstPtr& msg) {
+    odometry();
+}
+
+void odometry(float v_x, float v_y, float w, float time){
+
+}
 
 int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "odometry");  //per inizializzare il nodo
+    ros::NodeHandle n;                  //per inizializzare il nodo
+
+
+    ros::Subscriber odometry_sub = n.subscribe("cmd_vel", 1000, cmd_velCallback);
+
+    ros::spin();      //solo ROS, e' piu' efficiente perche' non considera ulteriori funzioni
+
 
     return 0;
 }
